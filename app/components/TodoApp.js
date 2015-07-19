df9c0049549c7f5f59c43b9b0434819030af5139
Mauricio Soares <m.soares2603@gmail.com>
2015-07-19 20:33:46 -0300

    update todoApp

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -1,16 +1,43 @@
 var React = require('react');
 var Footer = require('./Footer');
 var Header = require('./Header');
+var MainSection = require('./MainSection');
+var TodoStore = require('../stores/TodoStore');
 
-var a = {};
+function getTodoState() {
+  return {
+    allTodos: TodoStore.getAll(),
+    areAllComplete: TodoStore.areAllComplete()
+  };
+}
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return getTodoState();
+  },
+
+  componentDidMount: function() {
+    TodoStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function() {
+    TodoStore.removeChangeListener(this._onChange);
+  },
+
   render: function() {
     return(
       <div>
-        <Footer allTodos={a} />
+        <Header />
+        <MainSection
+          allTodos={this.state.allTodos}
+          areAllComplete={this.state.areAllComplete}/>
+        <Footer allTodos={this.state.allTodos} />
       </div>
     )
+  },
+
+  _onChange: function() {
+    this.setState(getTodosState());
   }
 });
 
